23ae61138ca82490164bc90e2c7006d68cf5409a
santiago <saforero88@gmail.com>
2021-03-23 16:25:07 -0500

    añadi a Vue el store

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Firebase from 'firebase'
-
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
+import store from "./store";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// Make BootstrapVue available throughout your project
+
 Vue.use(BootstrapVue)
-    // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
@@ -21,6 +18,7 @@ Firebase.auth().onAuthStateChanged(() => {
     if (!app) {
         new Vue({
             router,
+            store,
             render: h => h(App)
         }).$mount('#app')
     }
