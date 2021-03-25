3c76b093d322e1aedf0c02614336abc00b9f52ce
santiago <saforero88@gmail.com>
2021-03-25 12:50:59 -0500

    movi componenete de navegacion a Home

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <Navigation></Navigation>
-    </div>
     <router-view />
   </div>
 </template>
 
 <script>
-import Navigation from "@/components/layout/Navigation";
 export default {
-  components: {
-    Navigation,
-  },
+  components: {},
 };
 </script>
 <style>
