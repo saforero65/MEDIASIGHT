a7a87b065da7955c24e3efbe7a48c2eebde679ec
santiago <saforero88@gmail.com>
2021-03-25 12:54:01 -0500

    agrege menu nav a home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>Home</div>
+  <div class="body">
+    <Navigation></Navigation>
+    <div>Home</div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Navigation from "@/components/layout/Navigation";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Navigation,
+  },
 };
 </script>
+<style>
+.body {
+  background: lightblue;
+  height: 100vh;
+}
+</style>
