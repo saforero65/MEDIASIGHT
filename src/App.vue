d5afe528b0827c4f101bb437f4c62515b3f79de7
Santiago <saforero88@gmail.com>
2021-03-25 00:45:48 -0500

    quite estilos

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,6 @@ export default {
 };
 </script>
 <style>
-html,
-body {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-}
-body {
-  margin: 0px;
-}
-/* canvas {
- width: ;
-} */
 #app {
   height: 100%;
 }
