157085b8ff90f208ef3d68f6c03ca339e893b0ba
VanessaE124 <est.vanessa.escobar@unimilitar.edu.co>
2021-03-26 11:26:04 -0500

    Anomacion_Login

--- src/views/Fondo2.vue
+++ src/views/Fondo2.vue
@@ -6,106 +6,65 @@
 
 <script>
 // @ is an alias to /src
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { mapGetters } from "vuex";
+import * as Three from 'three';
+//import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+//import { mapGetters } from "vuex";
 
 export default {
-  name: "home",
+  name: 'ThreeTest',
   data() {
     return {
       camera: null,
       scene: null,
       renderer: null,
-      controls: null,
-      container: null,
-    };
-  },
-  computed: {
-    ...mapGetters({
-      model: "getModel",
-    }),
+      //controls: null,
+      mesh: null,
+      plane_mesh: null
+    }
   },
   methods: {
-    init: function () {
-      this.container = document.getElementById("container");
+    init: function() {
+        let container = document.getElementById('container');
 
-      this.renderer = new THREE.WebGLRenderer({ antialias: true });
-      this.renderer.setSize(
-        this.container.clientWidth,
-        this.container.clientHeight
-      );
-      this.container.appendChild(this.renderer.domElement);
+        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+        this.camera.position.z = 1;
+        //this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        //this.controls.screenSpacePanning = true;
 
-      this.scene = new THREE.Scene();
-      this.scene.background = new THREE.Color("black");
-      this.camera = new THREE.PerspectiveCamera(
-        45,
-        this.container.clientWidth / this.container.clientHeight,
-        1,
-        1000
-      );
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-      this.controls.screenSpacePanning = true;
+        this.scene = new Three.Scene();
 
-      var size = 10;
-      var divisions = 10;
+        let geometry = new Three.BoxGeometry(0.5,0.5,0.2);
+        let material = new Three.MeshNormalMaterial();
 
-      var gridHelper = new THREE.GridHelper(size, divisions);
-      gridHelper.position.y = -1.5;
-      this.scene.add(gridHelper);
+        this.mesh = new Three.Mesh(geometry, material);
+        this.mesh.translateX(0.7);
+        this.scene.add(this.mesh);
 
-      const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 2);
-      // const helper = new THREE.HemisphereLightHelper(light, 5);
-      this.scene.add(light);
+        var plane = new Three.PlaneGeometry(3,1);
+        var matPlane = new Three.MeshBasicMaterial( { color: 0xF6E3CE } );
+        this.plane_mesh = new Three.Mesh( plane, matPlane );
+        this.plane_mesh.rotateX(5);
+        this.plane_mesh.translateZ(-0.5);
+        this.scene.add(this.plane_mesh);
 
-      var directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
-      directionalLight2.position.set(10, -10, 10);
-      this.scene.add(directionalLight2);
+        this.renderer = new Three.WebGLRenderer({antialias: true});
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(this.renderer.domElement);
 
-      this.$store.dispatch("loadModel", "/three-assets/RobotExpressive.glb");
-
-      this.camera.position.z = 5;
-      // this.camera.position.x = 5;
     },
-    animate: function () {
-      requestAnimationFrame(this.animate);
-      // cube.rotation.x += 0.01;
-      // cube.rotation.y += 0.01;
-      this.renderer.render(this.scene, this.camera);
-    },
-    onWindowResize: function () {
-      console.log("resize");
-      this.camera.aspect =
-        this.container.clientWidth / this.container.clientHeight;
-      this.camera.updateProjectionMatrix();
 
-      this.renderer.setSize(
-        this.container.clientWidth,
-        this.container.clientHeight
-      );
-    },
-    addModel: function () {
-      this.$store.subscribe((mutation) => {
-        switch (mutation.type) {
-          case "setModel":
-            this.scene.add(this.model);
-            this.model.position.y = -1.5;
-            this.model.position.x = -3.5;
-            this.model.position.z = -3.5;
-            break;
-        }
-      });
-    },
+    animate: function() {
+        requestAnimationFrame(this.animate);
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
+    }
   },
   mounted() {
-    this.init();
-    this.animate();
-    this.addModel();
-
-    window.addEventListener("resize", this.onWindowResize, false);
-  },
-};
+      this.init();
+      this.animate();
+  }
+}
 </script>
 
 <style >
