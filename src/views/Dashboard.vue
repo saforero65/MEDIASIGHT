0888d648d0d7a47af521fd90d89296318d22c0f6
Santiago <saforero88@gmail.com>
2021-04-09 16:29:03 -0500

    dashboard con condicionales

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,49 @@
-<template>
-  <div>
-    <Navigation></Navigation>
-    Dashboard
+<template >
+  <div v-if="user">
+    <template v-if="user.email == 'admin@unimilitar.edu.co'">
+      <div>
+        perfil admin
+
+        <perfiladmin></perfiladmin>
+      </div>
+    </template>
+    <template v-else>
+      <div>
+        perfil miembro
+
+        <perfilmiembro></perfilmiembro>
+      </div>
+    </template>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import Navigation from "@/components/layout/Navigation";
+import Firebase from "firebase";
+import perfiladmin from "@/components/PerfilAdmin";
+import perfilmiembro from "@/components/PerfilMiembro";
 export default {
-  name: "Dashboard",
+  name: "dashboard",
   components: {
-    Navigation,
+    perfiladmin,
+    perfilmiembro,
+  },
+  data() {
+    return {
+      user: null,
+    };
+  },
+  created() {
+    Firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user;
+        console.log("this.user");
+        console.log(user);
+        console.log(user);
+      } else {
+        this.user = null;
+      }
+    });
   },
 };
 </script>
+<style>
+</style>
