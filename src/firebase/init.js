255915fb52517e05ccf248343e064a4e93a87139
Santiago <saforero88@gmail.com>
2021-04-23 17:18:28 -0500

    storage firebase

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,4 +18,5 @@ firebaseApp.firestore().settings({ timestampsInSnapshots: true })
 
 const db = firebase.firestore()
 const auth = firebase.auth()
-export { db, auth }
+const storage = firebase.storage()
+export { db, auth, storage }
